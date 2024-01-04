95cb920ba88b9d53d195580aa3e94971a32727e3
szbgreg <szbgreg@gmail.com>
2024-01-04 21:50:29 +0100

    Add input fields for minterm numbers and variable count

--- src/App.js
+++ src/App.js
@@ -1,9 +1,48 @@
 import React from 'react';
+import QuineMcCluskey from './QuineMcCluskey';
+import { sortingFn } from './utils';
 
 const App = () => {
+  const [number, setNumber] = React.useState('');
+  const [minterms, setMinterms] = React.useState('');
+  const [result, setResult] = React.useState('');
+
+  const onNumberChange = (e) => {
+    setNumber(e.target.value);
+  };
+
+  const onMintermsChange = (e) => {
+    setMinterms(e.target.value);
+  };
+
+  const onButtonClick = () => {
+    let mintermIndexes = minterms.split(',');
+    mintermIndexes = mintermIndexes.map((e) => Number(e)).sort(sortingFn);
+
+    const QMInstance = new QuineMcCluskey(number, mintermIndexes);
+    setResult(QMInstance.solve());
+  };
+
   return (
     <>
-      <div>Hello from React</div>
+      <div>
+        <label htmlFor='minterms'>Minterms</label>
+        <input
+          type='text'
+          id='minterms'
+          onChange={onMintermsChange}
+          value={minterms}
+        />
+        <label htmlFor='numberOfVariables'>Number of variables</label>
+        <input
+          type='number'
+          id='numberOfVariables'
+          onChange={onNumberChange}
+          value={number}
+        />
+        <input type='button' onClick={onButtonClick} value={'Solve'} />
+        {result}
+      </div>
     </>
   );
 };
